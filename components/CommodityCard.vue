53f0cf5d53058125b0413825d0808a15f162fa7b
Jacob Habib <habib.50@osu.edu>
2021-07-14 23:03:12 -0400

    added first component & blank page for coffee

---
+++ components/CommodityCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="relative flex flex-col items-center justify-center">
+    <div class="grid mt-8 gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2">
+      <div class="flex flex-col">
+        <div
+          class="
+            bg-white
+            shadow-md
+            rounded-3xl
+            p-4
+            transitionS
+            duration-500
+            ease-in-out
+            transform
+            hover:-translate-y-1
+            hover:scale-110
+          "
+        >
+          <div class="flex-none lg:flex">
+            <div class="h-full w-full lg:h-24 lg:w-48 lg:mb-0 mb-3">
+              <img
+                src="~/assets/beans.svg"
+                alt="{{"
+                commodity
+                }}
+                class="
+                  w-full
+                  object-scale-down
+                  lg:object-cover
+                  lg:h-24
+                  rounded-2xl
+                "
+              />
+            </div>
+            <div class="flex-auto mt-5 ml-3 justify-evenly py-1">
+              <div class="flex flex-wrap">
+                <h2 class="flex-auto text-4xl font-medium">{{ commodity }}</h2>
+              </div>
+              <p class="mt-3"></p>
+
+              <div class="flex p-4 pb-2 border-t border-gray-200"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: ["commodity"],
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <NuxtLink to="/coffee">
+      <CommodityCard commodity="Coffee" />
+    </NuxtLink>
+    <!-- <CommodityCard commodity = "Steel" />
+  <CommodityCard commodity = "WTI crude (oil)" />
+  <CommodityCard commodity = "Soybeans" />
+  <CommodityCard commodity = "Iron" />
+  <CommodityCard commodity = "Corn" />
+  <CommodityCard commodity = "Gold" />
+  <CommodityCard commodity = "Copper" />
+  <CommodityCard commodity = "Aluminum" />
+  <CommodityCard commodity = "Silver" />
+  <CommodityCard commodity = "Brent crude oil" /> -->
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
